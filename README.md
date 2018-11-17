31dd96516be499418eddd75444d14c7ea80a7bd1
Robert Young <robert.young@zeavo.com>
2018-11-17 15:06:42 +0000

    Removed provider from certification manager

--- README.md
+++ README.md
@@ -17,3 +17,32 @@ Terraform assumes that the domain name has already been registered on Route53.
 terraform init
 terraform apply
 ```
+
+main.tf
+
+```sh
+# CloudFront requires the certificate to be in us-east-1
+provider "aws" {
+  alias = "useast1"
+  region = "us-east-1"
+}
+
+module "s3-site-me-iamrobertyoung-co-uk" {
+  source = "github.com/robertyoung/terraform-aws-static-site-s3-https"
+
+  host_names = [
+    "example.co.uk",
+    "me.example.co.uk",
+    "www.example.co.uk",
+  ]
+
+  bucket_name = "me.example.co.uk"
+
+  domain_name = "example.co.uk"
+
+  additional_certificate_domains = [
+    "me.example.co.uk",
+    "www.example.co.uk",
+  ]
+}
+```
--- module/certificate-manager/main.tf
+++ module/certificate-manager/main.tf
@@ -1,7 +1,3 @@
-provider "aws" {
-  region = "us-east-1"
-}
-
 data "aws_route53_zone" "external" {
   name = "${var.domain_name}"
 }
@@ -10,6 +6,15 @@ resource "aws_acm_certificate" "default" {
   domain_name               = "${var.domain_name}"
   subject_alternative_names = "${var.additional_certificate_domains}"
   validation_method         = "DNS"
+  provider = "aws.useast1"
+}
+
+
+resource "aws_acm_certificate_validation" "default" {
+  count                   = "${length(var.additional_certificate_domains)}"
+  certificate_arn         = "${element(aws_acm_certificate.default.*.arn, count.index)}"
+  validation_record_fqdns = ["${aws_route53_record.validation.*.fqdn}"]
+  provider = "aws.useast1"
 }
 
 locals {
@@ -23,10 +28,4 @@ resource "aws_route53_record" "validation" {
   name    = "${lookup(local.dvo[count.index], "resource_record_name")}"
   type    = "${lookup(local.dvo[count.index], "resource_record_type")}"
   records = ["${lookup(local.dvo[count.index], "resource_record_value")}"]
-}
-
-resource "aws_acm_certificate_validation" "default" {
-  count                   = "${length(var.additional_certificate_domains)}"
-  certificate_arn         = "${element(aws_acm_certificate.default.*.arn, count.index)}"
-  validation_record_fqdns = ["${aws_route53_record.validation.*.fqdn}"]
 }

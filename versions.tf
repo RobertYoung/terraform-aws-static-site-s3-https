a1e0fd73f674a269058ea96e3605b527e771c32b
Robert Young <me@iamrobertyoung.co.uk>
2025-09-27 18:03:21 +0100

    Revert "chore(deps): bump the terraform-fix group across 5 directories with 1 update"
    This reverts commit 5eb7a27a53ff2b798c7508d27c56683a635305f5.

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     aws = {
       source                = "hashicorp/aws"
-      version               = "~> 6.14"
+      version               = "~> 5.0"
       configuration_aliases = [aws.useast1]
     }
   }
